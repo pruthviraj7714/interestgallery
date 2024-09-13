e895d21dec5f0b308f51747e0e8c1f3a48e2390e
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-09-13 23:45:02 +0530

    added shadcn

---
+++ src/components/Appbar.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Appbar() {
+    return (
+        <div className="h-20 flex justify-between items-center">
+            <div>
+                InterestGallery
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
